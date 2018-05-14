259e7b59f52ccba7eb72e4acb54600fcb2298f34
roymilnergithub <36075547+roymilnergithub@users.noreply.github.com>
2018-05-14 11:08:51 -0500

    New commit 1
    New commit 1

---
+++ inc/menu.php
@@ -0,0 +1,27 @@
+<header id="main-header">
+<div class= "container">
+	<div class="row">
+		<div class="col-xs-12">
+				
+				<p align="center"><span class="online-store"> ONLINE STORE</span> </p>
+		</div>
+
+		<div class="col-xs-2">
+		</div>
+		<div class="col-xs-9">
+			<ul id="main-menu" class="nav nav-pills">
+				<li><a href="index.php"> SERVICES </a> </li>
+				<li><a href="index.php"> 3D PRINTING </a> </li>
+				<li><a href="index.php"> 3D SCANNING </a> </li>
+				<li><a href="index.php"> LoT(INTERNET of THINGS) </a> </li>
+				<li><a href="index.php"> DRONES </a> </li>
+				<li><a href="index.php"> SMARTER TV </a> </li>
+				
+			</ul>
+		</div>
+		<div class="col-xs-1">
+		</div>
+
+	</div>
+</div>
+</header>
---
+++ index.php
@@ -0,0 +1,353 @@
+<!doctype html>
+<html>
+	<head>
+		<?php include 'inc/head_common.php'; ?>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		
+		
+		<!-- INDUSTRIAL CHIMP_IMAGE --> 
+		<article class="principal">
+			<div class="container">
+				<div class="row ">
+					<div class="col-xs-12 ">
+						<!-- Imagen -->
+						<link id="imagen-principal"><img src="img/industrial-chimp.png"  alt=""></link>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<br></br>
+						<p align="center"><link ><img src="img/chimp.png" width="100px" hight="100px" alt=""></link></p>
+						<h2 align="center">INDUSTRIAL CHIMP </h2>
+
+						<ul id="descripcion-empresa">
+							<li>
+								<p align="center"><a href="#"><span class="texto2"> 132A WASHINGTON ST. (SONO), NORWALK, CT 06854</span> </a></p>
+								<p align="center"><a href="#"><span class="texto2"> Closed — Opens tomorrow at 11:00 AM</span></a></p>
+								
+							</li>
+						</ul>
+
+						<p align="center" class="pickup">Pickup orders ready in 120 minutes</p>
+					</div>
+				</div>
+			</div>
+		</article>
+
+		<hr noshade="noshade" />
+		<?php include 'inc/menu.php'; ?>
+		<hr noshade="noshade" />
+		
+
+		<!-- PRODUCTS --> 
+		<section id="products">
+			
+				<!-- SERVICES --> 
+				<div class="container" >
+					<div class="row" id="services">
+						<div class="col-xs-12">
+							<h3>SERVICES</h3>			
+						</div>
+					</div>
+					<div class="row" >
+						<div class="col-xs-3 ">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/3d-printing-service.png" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">3D PRINTING SERVICE</a></li>
+								<li><a href="#">
+									<span class="price"> $10.00 – $50.00</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/3d-scanning-service.png" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">3D Scanning Service</a></li>
+								<li><a href="#">
+									<span class="price"> $10.00 – $200.00</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/repair-support-service.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">3D Scanning Service</a></li>
+								<li><a href="#">
+									<span class="price"> $45.00 – $75.00</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							
+						</div>
+					</div>
+				</div>
+				
+				<br></br>
+				<hr width="80%" />
+				<!-- 3D PRINTING --> 
+				<div class="container" >
+					<div class="row" id="services">
+						<div class="col-xs-12">
+							<h3>3D PRINTING</h3>			
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/m3d.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">m3D</a></li>
+								<li><a href="#">
+									<span class="price"> $408.10</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/3doodler-printing-pen.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">3Doodler Printing Pen</a></li>
+								<li><a href="#">
+									<span class="price"> $17.11 – $130.56</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/cube-3d-printer.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">CUBE 3D Printer</a></li>
+								<li><a href="#">
+									<span class="price"> $1,241.87</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/form1-resin3d.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">Form 1+ RESIN 3D Printer</a></li>
+								<li><a href="#">
+									<span class="price"> $76.26 – $3,799.49</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<br></br>
+					<!-- Segunda fila -->
+					<div class="row">
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/marketbot-replicator.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">Market Bot REPLICATOR</a></li>
+								<li><a href="#">
+									<span class="price"> $408.10</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/resin.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">RESIN</a></li>
+								<li><a href="#">
+									<span class="price"> $17.11 – $130.56</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/abs-filament.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">ABS Filament</a></li>
+								<li><a href="#">
+									<span class="price"> $1,241.87</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+						</div>
+					</div>
+
+				</div>
+
+				<br></br>
+				<hr width="80%" />
+				<!-- 3D SCANNING --> 
+				<div class="container" >
+					<div class="row" id="services">
+						<div class="col-xs-12">
+							<h3>3D SCANNING</h3>			
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/makerbot-digitizer.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">MakerBot Digitizer 3D scanner</a></li>
+								<li><a href="#">
+									<span class="price"> $408.10</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/isense-scanner.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">iSense 3D Scanner</a></li>
+								<li><a href="#">
+									<span class="price"> $17.11 – $130.56</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/kinect.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">Kinect</a></li>
+								<li><a href="#">
+									<span class="price"> $1,241.87</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<!-- Imagen -->
+							<link href="#"><img class="image-custom" src="img/fuel3d-scanify.jpg" alt=""></link>
+							<br></br>
+							<ul id="descripcion-producto">
+								<li><a href="#">fuel3D SCANIFY</a></li>
+								<li><a href="#">
+									<span class="price"> $76.26 – $3,799.49</span> 
+									<span class="texto"> Solo para recoger</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+		</section>
+
+		<hr noshade="noshade" />
+		<br></br>
+
+		<!-- FOOTER -->
+
+		<section id="table-of-contents">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-xs-4">
+						<h4 align="center">UBICACIÓN</h4>
+						<ul>
+							<li><p align="center">132A WASHINGTON ST. (SONO), NORWALK, CT 06854</p></li>
+							<li><p align="center">Cómo llegar</p></li>
+						</ul>
+					</div>
+					<div class="col-xs-4">
+						<h4 align="center">CONTÁCTANOS</h4>
+						<ul>
+							<li><p align="center">+1-860-245-9521</p></li>
+							<li><p align="center">industrialchimp.com</p></li>
+						</ul>
+
+					</div>
+					<div class="col-xs-4">
+						<section id="agenda">
+							<h4 align="center">HORARIO DE ATENCION</h4>
+							<dl class="dl-horizontal">
+								<dt>Lunes</dt>
+								<dd>11 AM – 8 PM</dd>
+								<dt>Martes</dt>
+								<dd>11 AM – 8 PM</dd>
+								<dt>Miercoles</dt>
+								<dd>11 AM – 8 PM</dd>
+								<dt>Jueves</dt>
+								<dd>11 AM – 8 PM</dd>
+								<dt>Viernes</dt>
+								<dd>11 AM – 8 PM</dd>
+								<dt>Sabado</dt>
+								<dd>11 AM – 8 PM</dd>
+								<dt>Domingo</dt>
+								<dd>11 AM – 8 PM</dd>
+							</dl>
+						</section>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<hr noshade="noshade" />
+		<!-- INDUSTRIAL CHIMP_IMAGE --> 
+		<article class="principal">
+			<div class="container" >
+					<div class="row">
+						<div class="col-xs-12">
+							<br></br>
+							<p align="center"><link ><img src="img/chimp.png" width="100px" hight="100px" alt=""></link></p>
+
+							<p align="center" class="pickup">Industrial chimp</p>
+						</div>
+					</div>
+			</div>
+		</article>
+
+		<hr noshade="noshade" />
+
+		<?php include 'inc/footer.php'; ?>
+		<?php include 'inc/footer_common.php'; ?>
+	</body>
+</html>
